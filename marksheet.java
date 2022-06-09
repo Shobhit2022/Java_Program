fcfe40d9c31ec67e4b3a35199fbaf34662c3cf47
Shobhit2022 <107178939+Shobhit2022@users.noreply.github.com>
2022-06-09 20:35:55 +0530

    Add files via upload

---
+++ marksheet.java
@@ -0,0 +1,32 @@
+import java.util.scanner;
+class Marksheet{
+
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner cm = new Scanner(system.in);
+
+        System.out.println("Enter Your Name:");
+        string name = cm.next();
+
+        System.out.println("Roll No:");
+        int Roll no = cm.next();
+
+        System.out.println("Enter Your Math Marks:");
+        int English = cm.next();
+
+        System.out.println("Enter Your Science Marks:");
+        int Hindi = cm.next();
+
+        System.out.println("Enter Your Biology Marks:");
+        int Maths = cm.next();
+
+        System.out.println("Enter Your Hindi Marks:");
+        int Science = cm.next();
+
+        System.out.println("Enter Your English Marks:");
+        int Drawing = cm.next();
+
+        System.out.println("The percentage is = " + percentage + " %")
+    }
+}
